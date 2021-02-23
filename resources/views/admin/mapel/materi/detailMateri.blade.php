8b0a4337d344c963886c3356b93a4c8eefb25834
afifwijaya16 <afif0071997@gmail.com>
2021-02-23 15:05:14 +0700

    update detail materi

--- resources/views/admin/mapel/materi/detailMateri.blade.php
+++ resources/views/admin/mapel/materi/detailMateri.blade.php
@@ -1,20 +1,13 @@
 @extends('template_backend/home')
 @section('sub-breadcrumb', 'Detail Materi')
 @section('content')
-<style>
-.embed-responsive {
-    position: relative;
-    display: block;
-    height: 0;
-    padding: 0;
-    overflow: hidden;
-}
-</style>
+
 <div class="row">
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <a href="{{ route('mapel.show', $mapel->id) }}" class="btn btn-xs btn-primary"><i class="fa fa-arrow-left"></i> Kembali</a>
+                <a href="{{ route('mapel.show', $mapel->id) }}" class="btn btn-xs btn-primary"><i
+                        class="fa fa-arrow-left"></i> Kembali</a>
             </div>
             <div class="card-body">
                 <div class="row">
@@ -33,39 +26,65 @@
                                 <td><b>{{ $mapel->hari_mapel }}, {{ $mapel->pukul_mapel }}</b></td>
                             </tr>
                             <tr>
+                                <td><b>Materi </b></td>
+                                <td> <a href="{{ asset($materi->berkas_materi) }}" target="_blank"
+                                        class="btn btn-sm bg-purple"><i class="fa fa-file"></i> Download Materi</a></td>
+                            </tr>
+                            <tr>
                                 <td><b>Keterangan</b></td>
                                 <td><b>{!! $materi->keterangan_materi !!}</b></td>
                             </tr>
                         </table>
                     </div>
                     <div class="col-sm-12">
-                        @if($materi->type_berkas_materi == "Image")
-                            <img src="{{ asset($materi->berkas_materi) }}" style="height:50px;width:50px;" class="img-fluid"/>
-                        @elseif($materi->type_berkas_materi == "PDF")
-                            <a href="{{ asset($materi->berkas_materi) }}" target="_blank" class="btn btn-sm bg-purple"><i class="fa fa-file"></i> Download Materi</a>
-                            <br>
-                            <br>
-                            <div class='embed-responsive' style='padding-bottom:150%'>
-                                <object data='{{ asset($materi->berkas_materi) }}' type='application/pdf' width='100%' height='100%'></object>
+                        <div class="media">
+                            <div class="media-body">
+
                             </div>
-                        @endif
+                        </div>
+                        <div class="timeline">
+                            <div>
+                                <i class="fas fa-video bg-maroon"></i>
+                                <div class="timeline-item">
+                                    <h3 class="timeline-header">Video Materi</h3>
+                                    <div class="timeline-body">
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            {!! Embed::make($materi->url_video_materi)->parseUrl()->getIframe() !!}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    @if($materi->kategori_materi == 'Soal')
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                
-            </div>
-            <div class="card-body">
-               
-            </div>
+        <div class="col-sm-12">
+            @if($materi->type_berkas_materi == "Image")
+            <img src="{{ asset($materi->berkas_materi) }}" style="height:50px;width:50px;" class="img-fluid" />
+            @elseif($materi->type_berkas_materi == "PDF")
+            <!-- <div class='embed-responsive' style='padding-bottom:150%'>
+                <object data='{{ asset($materi->berkas_materi) }}' type='application/pdf' width='100%'
+                    height='100%'></object>
+            </div> -->
+            @endif
         </div>
     </div>
-    @endif
+</div>
+</div>
+</div>
+@if($materi->kategori_materi == 'Soal')
+<div class="col-12">
+    <div class="card">
+        <div class="card-header">
+
+        </div>
+        <div class="card-body">
+
+        </div>
+    </div>
+</div>
+@endif
 </div>
 @push('js')
 @endpush
